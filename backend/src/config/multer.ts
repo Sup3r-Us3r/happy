6a279ce92e4bbce5572ffa1bc03ad573f456cad4
Sup3r-Us3r <maydersonmello@gmail.com>
2020-10-28 09:00:03 -0300

    disable validation

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -3,22 +3,22 @@ import {resolve} from 'path';
 import {randomBytes} from 'crypto';
 
 const multerConfig = {
-  fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      'image/jpeg',
-      'image/jpg',
-      'image/png',
-    ];
+  // fileFilter: (req, file, cb) => {
+  //   const allowedMimes = [
+  //     'image/jpeg',
+  //     'image/jpg',
+  //     'image/png',
+  //   ];
 
-    if (!allowedMimes.includes(file.mimetype)) {
-      return cb(new Error('Invalid file type.'));
-    }
+  //   if (!allowedMimes.includes(file.mimetype)) {
+  //     return cb(new Error('Invalid file type.'));
+  //   }
 
-    return cb(null, true);
-  },
-  limits: {
-    fileSize: 8 * 1024 * 1024,
-  },
+  //   return cb(null, true);
+  // },
+  // limits: {
+  //   fileSize: 8 * 1024 * 1024,
+  // },
   storage: multer.diskStorage({
     destination: resolve(__dirname, '..', '..', 'uploads'),
     filename: (req, file, cb) => {
